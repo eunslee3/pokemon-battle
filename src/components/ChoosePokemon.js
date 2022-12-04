298872fbba5b4561252972d43d222b28d56df024
Samuel <lee.eunseong7856@gmail.com>
2022-12-03 23:19:58 -0600

    ability to switch through images

--- src/components/ChoosePokemon.js
+++ src/components/ChoosePokemon.js
@@ -6,16 +6,41 @@ function ChoosePokemon({setSelectPokemon}) {
     const [showVenusaur, setShowVenusaur] = useState(false)
     const [showCharzard, setShowCharzard] = useState(false)
 
+    console.log(showBlastoise)
+    console.log(showVenusaur)
+    console.log(showCharzard)
+
     function showSelectedPokemon() {
         if(showBlastoise === true) {
             return(
-                <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/4/4a/Blastoise.gif" alt="daBlastoise"/>
+                <div className="displayedPokemonContainer">
+                    <img className="displayedPokemon" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/4/4a/Blastoise.gif" alt="daBlastoise"/>
+                    <h3>Blastoise</h3>
+                </div>
             )
         }
         if(showVenusaur === true) {
             return (
-                <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-omega-ruby-and-alpha-sapphire/1/12/Venusaur.jpg" alt="daVenusaur"/>
+                <div className="displayedPokemonContainer">
+                    <img className="displayedPokemon" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-omega-ruby-and-alpha-sapphire/1/12/Venusaur.jpg" alt="daVenusaur"/>
+                    <h3>Venusaur</h3>
+                </div>
             )
+        }
+        if(showCharzard === true) {
+            return (
+                <div >
+                    <div className="displayedPokemonContainer">
+                        <img className="displayedPokemon" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/6/69/Charizard.jpg" alt="daCharzard"/>
+                    </div>
+                    <div className="displayedDescriptionText">
+                        <h3>Charzard</h3>
+                    </div>
+                </div>
+            )
+        }
+        else{
+            return null
         }
     }
 
@@ -31,7 +56,7 @@ function ChoosePokemon({setSelectPokemon}) {
                 setShowCharzard={setShowCharzard}
             />
             <div className="showPokemon">
-        
+                {showSelectedPokemon()}
             </div>
             <div id="parent-start-button">
                 <button className="button">Start Battle!</button>
